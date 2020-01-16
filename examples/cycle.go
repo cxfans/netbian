547726176c8468f7b8e467c606fd87d450c0705d
Sheila Wendler <seeknewaddrs@gmail.com>
2020-01-16 11:05:07 +0800

    examples/cycle.go: improve code structure

--- examples/cycle.go
+++ examples/cycle.go
@@ -1,6 +1,6 @@
 /*
 	彼岸图网（http://pic.netbian.com/）爬虫
-	由于网站限制终身会员每天最多下载200张图片，所以制定了此周期性爬取方案
+	网站限制终身会员每天最多下载200张图片，因此制定了此周期性爬取方案
 */
 
 package main
@@ -11,15 +11,22 @@ import (
 	"time"
 )
 
+const (
+	start = 19706
+	stop  = 25381
+)
+
+// go build -o bin/cycle examples/cycle.go
 func main() {
-	fmt.Println("========= GO =========")
-	for d := 15726; d <= 25100; d += 200 {
+	for d := start; d <= stop; d += 200 {
 		for n := d; n < d+200; n++ {
-			_ = netbian.Crawler(n, fmt.Sprintf("/Users/rustle/Desktop/Pictures/netbian/%d.jpg", n))
+			_ = netbian.Crawler(n, fmt.Sprintf("/imgs/%d.jpg", n))
 			fmt.Println("DOWNLOAD: ", n)
 			time.Sleep(1 * time.Second)
 		}
-		time.Sleep(24 * time.Hour)
+		fmt.Println("Today's mission has been completed.")
+		if d <= stop {
+			time.Sleep(24 * time.Hour)
+		}
 	}
-	fmt.Println("========= OK =========")
 }
